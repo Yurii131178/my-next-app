edac2611e7b6f506e4bc2dc0d3a8d2fe0069c291
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-02 16:32:30 +0300

    update NoteForm

--- app/(public routes)/notes/action/create/page.tsx
+++ app/(public routes)/notes/action/create/page.tsx
@@ -1,12 +1,19 @@
+// app/notes/action/create
+
 /**Сторінка створення нотатки
 
-Почнемо зі сторінки за шляхом /notes/action/create/page.tsx. */
+1. Почнемо зі сторінки за шляхом /notes/action/create/page.tsx. */
 
+import NoteForm from '@/components/NoteForm/NoteForm';
 import { getCategories } from '@/lib/api';
 
 const CreateNote = async () => {
-  const categories = await getCategories;
-  return <div>CreateNote</div>;
+  const categories = await getCategories();
+  return (
+    <>
+      <NoteForm categories={categories} />
+    </>
+  );
 };
 
 export default CreateNote;
--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -1,9 +1,69 @@
+// // components/NoteForm/NoteForm.tsx
+
+// 'use client';
+
+// import { useRouter } from 'next/navigation';
+// import { Category } from '@/lib/api';
+
+// type Props = {
+//   categories: Category[];
+// };
+
+// const NoteForm = ({ categories }: Props) => {
+//   const router = useRouter();
+
+//   const handleCancel = () => router.push('/notes/filter/all');
+
+//   const handleSubmit = (formData: FormData) => {
+//     const values = Object.fromEntries(formData);
+//     console.log(values);
+//   };
+
+//   return (
+//     <form action={handleSubmit}>
+//       <label>
+//         Title
+//         <input type="text" name="title" />
+//       </label>
+
+//       <label>
+//         Content
+//         <textarea name="content"></textarea>
+//       </label>
+
+//       <label>
+//         Category
+//         <select name="category">
+//           {categories.map((category) => (
+//             <option key={category.id} value={category.id}>
+//               {category.name}
+//             </option>
+//           ))}
+//         </select>
+//       </label>
+
+//       <div>
+//         <button type="submit">Create</button>
+//         <button type="button" onClick={handleCancel}>
+//           Cancel
+//         </button>
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default NoteForm;
+
+/////////////
+
 // components/NoteForm/NoteForm.tsx
 
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { Category } from '@/lib/api';
+import { useMutation } from '@tanstack/react-query';
+import { Category, createNote, NewNoteData } from '@/lib/api';
+import css from './NoteForm.module.css';
 
 type Props = {
   categories: Category[];
@@ -12,28 +72,40 @@ type Props = {
 const NoteForm = ({ categories }: Props) => {
   const router = useRouter();
 
+  const { mutate } = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      router.push('/notes/filter/all');
+    },
+  });
+
   const handleCancel = () => router.push('/notes/filter/all');
 
   const handleSubmit = (formData: FormData) => {
     const values = Object.fromEntries(formData);
-    console.log(values);
+    const noteData: NewNoteData = {
+      title: values.title as string,
+      content: values.content as string,
+      categoryId: values.categoryId as string,
+    };
+    mutate(noteData);
   };
 
   return (
-    <form action={handleSubmit}>
-      <label>
+    <form className={css.form} action={handleSubmit}>
+      <label className={css.label}>
         Title
-        <input type="text" name="title" />
+        <input type="text" name="title" className={css.input} />
       </label>
 
-      <label>
+      <label className={css.label}>
         Content
-        <textarea name="content"></textarea>
+        <textarea name="content" className={css.textarea}></textarea>
       </label>
 
-      <label>
+      <label className={css.label}>
         Category
-        <select name="category">
+        <select name="categoryId" className={css.select}>
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
               {category.name}
@@ -42,9 +114,11 @@ const NoteForm = ({ categories }: Props) => {
         </select>
       </label>
 
-      <div>
-        <button type="submit">Create</button>
-        <button type="button" onClick={handleCancel}>
+      <div className={css.buttonGroup}>
+        <button type="submit" className={css.submitBtn}>
+          Create
+        </button>
+        <button type="button" onClick={handleCancel} className={css.cancelBtn}>
           Cancel
         </button>
       </div>
