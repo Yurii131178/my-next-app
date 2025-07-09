ff5d24cb4d7e6a5aed051c4ab2a1cd3a9584ab50
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-07-09 16:09:36 +0300

    creat logout/route.ts_update AuthNavigation woth the real logout logics

--- components/AuthNavigation/AuthNavigation.tsx
+++ components/AuthNavigation/AuthNavigation.tsx
@@ -1,16 +1,66 @@
+// 'use client';
+
+// import Link from 'next/link';
+// import { useAuthStore } from '../../lib/stores/authStore';
+
+// const AuthNavigation = () => {
+//   // Отримуємо поточну сесію та юзера
+//   const { isAuthenticated, user } = useAuthStore();
+
+//   const handleLogout = () => {};
+
+//   // Якщо є сесія - відображаємо Logout та інформацію про користувача
+//   // інакше - посилання на логін та реєстрацію
+//   return isAuthenticated ? (
+//     <li>
+//       <p>{user?.email}</p>
+//       <button onClick={handleLogout}>Logout</button>
+//     </li>
+//   ) : (
+//     <>
+//       <li>
+//         <Link href="/sign-in">Login</Link>
+//       </li>
+//       <li>
+//         <Link href="/sign-up">Register</Link>
+//       </li>
+//     </>
+//   );
+// };
+
+// export default AuthNavigation;
+
+//// Оновлюємо AuthNavigation, щоб додати справжню дію логауту до handleLogout: //
+
+// components/AuthNavigation/AuthNavigation.tsx
+
 'use client';
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useAuthStore } from '../../lib/stores/authStore';
+import { logout } from '@/lib/api';
 
 const AuthNavigation = () => {
+  const router = useRouter();
   // Отримуємо поточну сесію та юзера
   const { isAuthenticated, user } = useAuthStore();
+  // Отримуємо метод очищення глобального стану
+  const clearIsAuthenticated = useAuthStore(
+    (state) => state.clearIsAuthenticated,
+  );
 
-  const handleLogout = () => {};
+  const handleLogout = async () => {
+    // Викликаємо logout
+    await logout();
+    // Чистимо глобальний стан
+    clearIsAuthenticated();
+    // Виконуємо навігацію на сторінку авторизації
+    router.push('/sign-in');
+  };
 
-  // Якщо є сесія - відображаємо Logout та інформацію про користувача
-  // інакше - посилання на логін та реєстрацію
+  // Якщо є сесія - відображаємо кнопку Logout та інформацію про користувача
+  // інакше - лінки для авторизації
   return isAuthenticated ? (
     <li>
       <p>{user?.email}</p>
@@ -22,7 +72,7 @@ const AuthNavigation = () => {
         <Link href="/sign-in">Login</Link>
       </li>
       <li>
-        <Link href="/sign-up">Register</Link>
+        <Link href="/sign-up">Sign up</Link>
       </li>
     </>
   );
